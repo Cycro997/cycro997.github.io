2c3a6277b5abd377412293a96352bcd7f3459370
Cycro997 <152512005+Cycro997@users.noreply.github.com>
2024-05-12 21:01:14 +0300

    {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}

--- .other/futurerp/timeconv/index.js
+++ .other/futurerp/timeconv/index.js
@@ -1,9 +1,16 @@
 "use strict";
+function convertTZ(date, tzString="utc") {
+    return new Date(
+        (typeof date === "string" ? new Date(date) : date)
+        .toLocaleString("en-US", {timeZone: tzString})
+    );   
+}
 function getDate(date){
+    date = convertTZ(date)
     function toDay(d){
         return Math.floor(d.getTime()/86400000)
     }
-    const start = toDay(new Date("2024-04-09"));
+    const start = toDay(convertTZ(new Date("2024-04-09")));
     let day = toDay(date) - start;
     
     const rmonth = day % 12;
@@ -14,14 +21,9 @@ function getDate(date){
         "Jun", "Jul", "Aug", "Sep", "Oct",
         "Nov", "Dec"
     ]
-    const monthlen = [
-        31, 28, 31, 30, 31, 30, 31, 31, 30,
-        31, 30, 31
-    ]
     const nmon = months[rmonth]
-    const monl = monthlen[rmonth]
     
-    const rday = Math.floor(date.getHours() / 24 * monl) + 1
+    const rday = Math.ceil(date.getHours() / 24 * 30) + 1
     
     if (ryear < 0){
         return `${-ryear}BC`
@@ -31,13 +33,19 @@ function getDate(date){
     }
     return `${ryear}`
 }
+
 const r = getDate(new Date());
 document.getElementById("ctime").innerHTML =
 `Current: ${r}`
 
 function changeOption(){
     const date = document.getElementById("enter").value;
+    const time = document.getElementById("entert").value;
+
+    if (!(date && time)){
+        return;
+    }
     
     document.getElementById("res").innerHTML =
-    `Output: ${getDate(new Date(date))}`
+    `Output: ${getDate(new Date(`${date} ${time}`))}`
 }
